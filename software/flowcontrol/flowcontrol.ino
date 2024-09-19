b809ef213241b8db4758e4e4ee84a9539e0386d3
Banana Man <Jake4971@gmail.com>
2024-09-19 12:06:24 -0400

    prepared kicad and arduino files
    Both files are very rudimentary. We'll eventually add psuedocode and fiddle with the components in the schematic so that we'll make these files proper.

---
+++ software/flowcontrol/flowcontrol.ino
@@ -0,0 +1,15 @@
+#define F_CPU 8000000L
+#include <avr/io.h>
+#include <util/delay.h>
+
+void setup() {
+  //Set up the flow controller.
+  // Light up ALL of the LEDs for 2 seconds as a sign that it's on.
+  
+
+}
+
+void loop() {
+  
+
+}
